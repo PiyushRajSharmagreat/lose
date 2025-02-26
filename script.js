4eb045d4b4719b036771c9a79c98485143fa0799
PiyushRajSharmagreat <rockstar.piyushrajsharma@gmail.com>
2025-02-26 19:48:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ addEventListener("load",async ()=>{
 async function subscribe() {
   let sw  = await navigator.serviceWorker.ready
   let push = sw.pushManager.subscribe({
-    userVisibleOnly: "true",
+    userVisibleOnly: true,
     applicationServerKey : "BPN4TRRiVwfzfamCK1-VVBvxiy4uQRf4_RvmGFHuQdd0GSW5pPM6CpIjhGZrbubuddCoHwdOk-5WgHzu0njlAiw"
   })
 
